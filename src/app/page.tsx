f9eb28503690c03ffb5c7b48b082c91171f952cc
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-27 10:22:39 +0000

    apost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <p>You're looking at my root route! Rood!</p>
+     <p>You are looking at my root route! Rood!</p>
     </main>
   )
 }
