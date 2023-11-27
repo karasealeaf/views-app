98183970b03f70d84233abe93bc3246db86fb19f
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-27 11:12:51 +0000

    code from Sam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from 'next/image'
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  await sql`INSERT INTO ViewsTable (views) SELECT 0 WHERE NOT EXISTS (SELECT * FROM ViewsTable)`;
+
+  await sql`UPDATE ViewsTable SET views = views =1`;
+
+  const result = await sql`SELECT views from ViewsTable`;
+
+  console.log(result);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <p>You are looking at my root route! Rood!</p>
+      <p>You&apos;re looking at my root route! Rood!</p>
     </main>
-  )
+  );
 }
